f37752f4d6f3bc251f5aea7728ee3b28e6c21b73
Mani1994Ganesh <maniganesh.nunna@gmail.com>
2024-10-05 16:11:51 +0530

    rating and Course Certification

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./styles.css";
 import React from "react";
 import Navbar from "./components/Navbar";
 import SessionPage from "./components/SessionPage";
-
+import Certification from "./components/Certification";
 export default function App() {
   return (
     <div className="App">
       <div className="container-fluid">
         <Navbar />
         <SessionPage />
+        <Certification/>
       </div>
     </div>
   );
